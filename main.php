d34f92f5aada435d2e182cecfe1225a002ea2a81
Pauline Ramelet <pauline.ramelet@icloud.com>
2026-06-17 17:20:57 +0200

    Finalisation commande list

--- main.php
+++ main.php
@@ -1,9 +1,23 @@
 <?php
+
+require_once 'DBConnect.php';
+require_once 'Contact.php';
+require_once 'ContactManager.php';
+
+$db = new DBConnect();
+$pdo = $db->getPDO();
+
+
 while (true) {
     $line = readline("Entrez votre commande : ");
     echo "Vous avez saisi : $line\n";
 
     if ($line === "list") {
-        echo "affichage de la liste\n";
+        $manager = new ContactManager($pdo);
+        $contacts = $manager->findAll();
+
+        foreach ($contacts as $contact) {
+            echo $contact->toString() . "\n";
+        }
     }
 }
