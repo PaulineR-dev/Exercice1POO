32fe8a3dcf802bfc9052b99327a463391d24c638
Pauline Ramelet <pauline.ramelet@icloud.com>
2026-06-17 13:06:03 +0200

    Création de la commande list

--- main.php
+++ main.php
@@ -2,4 +2,8 @@
 while (true) {
     $line = readline("Entrez votre commande : ");
     echo "Vous avez saisi : $line\n";
+
+    if ($line === "list") {
+        echo "affichage de la liste\n";
+    }
 }
