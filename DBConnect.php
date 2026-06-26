1aa1128349e40642d23bd460b75b67005e21fe11
Pauline Ramelet <pauline.ramelet@icloud.com>
2026-06-26 09:38:10 +0200

    Ajout .gitignore et externalisation des identifiants de connexion à la base de données

--- DBConnect.php
+++ DBConnect.php
@@ -7,10 +7,13 @@ class DBConnect
 {
     public static function getPDO(): PDO
     {
+        // Chargement du fichier avec les identifiants de connexion à la base de données
+        $config = require __DIR__ . '/../connexion.php';
+
         return new PDO(
-            "mysql:host=localhost;dbname=contacts;charset=utf8mb4",
-            "root",
-            "",
+            "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8mb4",
+            $config['user'],
+            $config['password']
         );
     }
 }
