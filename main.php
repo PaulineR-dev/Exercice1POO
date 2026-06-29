206fb8c07d992d289efe16da06dcdc874dc76e4b
Pauline Ramelet <pauline.ramelet@icloud.com>
2026-06-29 17:25:12 +0200

    Modification identation elseif

--- main.php
+++ main.php
@@ -17,42 +17,29 @@ while (true) {
 
     if ($line === "list") {
         $command->list();
-    }
-
-    elseif (preg_match('/^detail (\d+)$/', $line, $matches)) {
+    } elseif (preg_match('/^detail (\d+)$/', $line, $matches)) {
         $id = (int)$matches[1];
         $command->detail($id);
-    }
-
-    elseif (preg_match('/^create\s+([^,]+),\s*([^,]+),\s*(.+)$/', $line, $matches)) {
+    } elseif (preg_match('/^create\s+([^,]+),\s*([^,]+),\s*(.+)$/', $line, $matches)) {
         $name = trim($matches[1]);
         $email = trim($matches[2]);
         $phone = trim($matches[3]);
         $command->create($name, $email, $phone);
-    }
-
-    elseif (preg_match('/^delete (\d+)$/', $line, $matches)) {
+    } elseif (preg_match('/^delete (\d+)$/', $line, $matches)) {
         $id = (int)$matches[1];
         $command->delete($id);
-    }
-
-    elseif (preg_match('/^modify\s+(\d+),\s*([^,]+),\s*([^,]+),\s*(.+)$/', $line, $matches)) {
+    } elseif (preg_match('/^modify\s+(\d+),\s*([^,]+),\s*([^,]+),\s*(.+)$/', $line, $matches)) {
         $id = (int)$matches[1];
         $name = trim($matches[2]);
         $email = trim($matches[3]);
         $phone = trim($matches[4]);
         $command->modify($id, $name, $email, $phone);
-    }
-
-    elseif ($line === "help") {
+    } elseif ($line === "help") {
         $command->help();
-    }
-
-    elseif ($line === "quit") {
+    } elseif ($line === "quit") {
         echo "Fermeture du programme.\n";
         break;
     }
-
     else {
         echo "Commande inconnue ou mal formatée.\n";
     }
